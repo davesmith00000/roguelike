acc8f8308422e8e9bf854d3bf9828b130ccaea2d
David North <hobnob@users.noreply.github.com>
2022-09-14 21:32:11 +0100

    Updates menu slide in
    Still loads to do to the menu system - but not a bad start

--- src/main/scala/roguelike/extensions/Group.scala
+++ src/main/scala/roguelike/extensions/Group.scala
@@ -3,91 +3,91 @@ package roguelike.extensions
 import indigo._
 import roguelike.Math
 
-extension [M <: Material](t: Text[M])
-  def lerp(start: Point, end: Point, percentage: Double): Text[M] =
+extension (g: Group)
+  def lerp(start: Point, end: Point, percentage: Double): Group =
     if (percentage <= 1)
-      t.moveTo(
+      g.withPosition(
         Point(
           Math.lerp(start.x.toDouble, end.x.toDouble, percentage).toInt,
           Math.lerp(start.y.toDouble, end.y.toDouble, percentage).toInt
         )
       )
     else
-      t.moveTo(end)
+      g.withPosition(end)
 
   def lerp(
       start: Point,
       end: Point,
       duration: Seconds,
       time: Seconds
-  ): Text[M] =
+  ): Group =
     if (time <= duration)
       lerp(start, end, time.toFloat / duration.toFloat)
     else
-      t.moveTo(end)
+      g.withPosition(end)
 
-  def easeIn(start: Point, end: Point, percentage: Double): Text[M] =
+  def easeIn(start: Point, end: Point, percentage: Double): Group =
     if (percentage <= 1)
-      t.moveTo(
+      g.withPosition(
         Point(
           Math.easeIn(start.x.toDouble, end.x.toDouble, percentage).toInt,
           Math.easeIn(start.y.toDouble, end.y.toDouble, percentage).toInt
         )
       )
     else
-      t.moveTo(end)
+      g.withPosition(end)
 
   def easeIn(
       start: Point,
       end: Point,
       duration: Seconds,
       time: Seconds
-  ): Text[M] =
+  ): Group =
     if (time <= duration)
       easeIn(start, end, time.toDouble / duration.toDouble)
     else
-      t.moveTo(end)
+      g.withPosition(end)
 
-  def easeOut(start: Point, end: Point, percentage: Double): Text[M] =
+  def easeOut(start: Point, end: Point, percentage: Double): Group =
     if (percentage <= 1)
-      t.moveTo(
+      g.withPosition(
         Point(
           Math.easeOut(start.x.toDouble, end.x.toDouble, percentage).toInt,
           Math.easeOut(start.y.toDouble, end.y.toDouble, percentage).toInt
         )
       )
     else
-      t.moveTo(end)
+      g.withPosition(end)
 
   def easeOut(
       start: Point,
       end: Point,
       duration: Seconds,
       time: Seconds
-  ): Text[M] =
+  ): Group =
     if (time <= duration)
       easeOut(start, end, time.toFloat / duration.toFloat)
     else
-      t.moveTo(end)
+      g.withPosition(end)
 
-  def easeInOut(start: Point, end: Point, percentage: Double): Text[M] =
+  def easeInOut(start: Point, end: Point, percentage: Double): Group =
     if (percentage <= 1)
-      t.moveTo(
+      g.withPosition(
         Point(
           Math.easeInOut(start.x.toDouble, end.x.toDouble, percentage).toInt,
           Math.easeInOut(start.y.toDouble, end.y.toDouble, percentage).toInt
         )
       )
     else
-      t.moveTo(end)
+      g.withPosition(end)
 
   def easeInOut(
       start: Point,
       end: Point,
       duration: Seconds,
       time: Seconds
-  ): Text[M] =
+  ): Group =
     if (time <= duration)
       easeInOut(start, end, time.toFloat / duration.toFloat)
     else
-      t.moveTo(end)
+      g.withPosition(end)
